2b6e378609d1d5fc809917cae3bf920161deb012
pedro bassani <pedrobassani98@gmail.com>
2019-10-10 08:55:39 -0300

    Add project files.

---
+++ MeuEcommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MeuEcommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
